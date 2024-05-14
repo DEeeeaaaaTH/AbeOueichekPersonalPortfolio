308968684117d621542e14fe09b7e73c52032c68
DEeeeaaaaTH <abdallah.oueichek@gmail.com>
2024-05-14 05:55:30 -0700

    Deployed Website

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import Logo from '../../assets/logo.jpg'
 import data from './data'
-import {IoIosColorPalette} from 'react-icons/io'
+import { IoColorFill } from "react-icons/io5";
 import { useModalContext } from '../../context/modal-context'
 import './navbar.css'
 
@@ -18,7 +18,7 @@ const Navbar = () => {
             data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
           }
         </ul>
-        <button id='theme__icon' onClick={showModalHandler}><IoIosColorPalette/></button>
+        <button id='theme__icon' onClick={showModalHandler}><IoColorFill /></button>
       </div>
     </nav>
   )
